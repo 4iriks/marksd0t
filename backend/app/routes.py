a71fbaca86553e8de22cbe6c1e933d4e1efe1640
4iriks <lexa_vordak@mail.ru>
2025-12-14 00:15:45 +0300

    Добавлены импорты для работы с БД и моделями

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,3 +1,6 @@
 from flask import Blueprint, request, jsonify
+from sqlalchemy.orm import Session
+from .database import get_db
+from .models import Note, Tag
 
 api = Blueprint('api', __name__, url_prefix='/api')
