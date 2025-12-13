ef0cd8d49048ced1a5cf3a9dc3add4320ab00132
4iriks <lexa_vordak@mail.ru>
2025-12-14 01:35:23 +0300

    Начата реализация endpoint PUT /notes/<id>

---
+++ backend/app/routes.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, request, jsonify
+from sqlalchemy.orm import Session
+from .database import get_db
+from .models import Note, Tag
+from datetime import datetime
+
+api = Blueprint('api', __name__, url_prefix='/api')
+
+
+@api.route('/notes/<int:note_id>', methods=['PUT'])
+def update_note(note_id):
+    """Обновить заметку"""
+    db = next(get_db())
+    note = db.query(Note).filter(Note.id == note_id).first()
