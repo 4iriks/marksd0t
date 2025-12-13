bfbdeae9b6d71310e84e0da91d15956a7158c3ab
4iriks <lexa_vordak@mail.ru>
2025-12-14 00:16:36 +0300

    Начата реализация endpoint GET /notes

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -4,3 +4,11 @@ from .database import get_db
 from .models import Note, Tag
 
 api = Blueprint('api', __name__, url_prefix='/api')
+
+
+@api.route('/notes', methods=['GET'])
+def get_notes():
+    """Получить все заметки с фильтрацией"""
+    db = next(get_db())
+    
+    query = db.query(Note)
